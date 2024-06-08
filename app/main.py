080c5c99180c6ca015370a9b7809403819859769
Netanel <netanelxa@gmail.com>
2024-06-08 09:22:05 +0000

    added html templte

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
-
+from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+#@app.get("/")
+#def read_root():
+#    return {"Hello": "World"}
 
+app.mount("/", StaticFiles(directory="./app//static",html = True), name="static")
 
